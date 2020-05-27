ba1f55fe243f69136f77e726c070046a1dc08d1f
Eskindir sophonias <eskesophe@gmail.com>
2020-05-27 18:52:24 +0200

    adding text of cv

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,145 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+      <div class="inner">
+        <nav class="nav nav-masthead justify-content-center">
+          <a class="nav-link active" href="#">Home</a>
+          <a class="nav-link" href="mailto:eskesophe@gmail.com">Contact</a>&emsp;
+          <span style="background-color: blueviolet; border-radius: 60px;padding: 5px;">LA</span>&emsp;
+          <a  href="en.html"> <img  src="image/Kingdom.png" height="15px" style="width: 20px"></a>&emsp;
+          <a href="index.html"> <img  src="image/France.png" height="15px" style="width: 20px"></a>
+        </nav>
+      </div>
+    </header>
+    
+    <main role="main" class="inner cover">
+      <div class="px-4  my-4 bg-white" id="headstyle">
+      <h1 id="metal" class="cover-heading mt-5">Eskindir Sophonias</h1>
+      <p class="text-dark">
+        Place du tunnel 5 <br>
+        1005 Lausanne <br>
+        <a style="color: black;" href="mailto:eskesophe@gmail.com">eskesophe@gmail.com</a><br>
+        079 838 01 71
+      </p></div>
+      <h2 class="px-4 bg-white text-dark" style="border-radius: 2px;">Expérience </h2>
+      <p> <b>En cours:</b>, Design et production d'objects, FABLAB/Renens</p>
+      <p> <b>2018: Assistant au département électricité</b>, Festival Image/Vevey</p>
+      <p> <b>2017: Assistant de laboratoire</b>, Tests sur les nouvelles machines à thé et à café, Nestlé/Orbe</p>
+      <p> <b>2010-12: Employé au sein de l'entreprise familiale</b>, Addis Ababa, Ethiopie</p>
+      <h2 class="px-4 bg-white text-dark" style="border-radius: 2px;"> Education </h2>
+      <p> <b>2019: Powercoders-Lausanne</b>, Programmation Internet (Front-End), Lausanne</p>
+      <p> <b>2017: Cap Horizon</b>, Cours de français, maths, Microsoft Word, Excel et Powerpoint, Lausanne</p>
+      <p> <b>2016: Pôle Sud (Centre socioculturel de l’USV)</b> Cours de français, Lausanne</p>
+      <p> <b>2013-15: Evam</b> Cours de français, Renens</p>
+      <p> <b>1999-08: École obligatoire</b>, Addis Ababa, Ethiopie</p>
+      <hr class="my-4">
+      <div class="row">
+        <div class="side">
+          <div> Html/Css</div>
+        </div>
+        <div class="middle">
+          <div class="bar-container">
+            <div class="bar-5"></div>
+          </div>
+        </div>
+        <div class="side right">
+          <div>5</div>
+        </div>
+        <div class="side">
+          <div>Sketch-Up</div>
+        </div>
+        <div class="middle">
+          <div class="bar-container">
+            <div class="bar-4"></div>
+          </div>
+        </div>
+        <div class="side right">
+          <div>5</div>
+        </div>
+        <div class="side">
+          <div>Bootstrap</div>
+        </div>
+        <div class="middle">
+          <div class="bar-container">
+            <div class="bar-3"></div>
+          </div>
+        </div>
+        <div class="side right">
+          <div>4</div>
+        </div>
+        <div class="side">
+          <div>JavaScript</div>
+        </div>
+        <div class="middle">
+          <div class="bar-container">
+            <div class="bar-2"></div>
+          </div>
+        </div>
+        <div class="side right">
+          <div>3.5</div>
+        </div>
+        <div class="side">
+          <div>PHP</div>
+        </div>
+        <div class="middle">
+          <div class="bar-container">
+            <div class="bar-1"></div>
+          </div>
+        </div>
+        <div class="side right">
+          <div>3</div>
+        </div>
+        <div class="side">
+          <div>Ms-office</div>
+        </div>
+        <div class="middle">
+          <div class="bar-container">
+            <div class="bar-1"></div>
+          </div>
+        </div>
+        <div class="side right">
+          <div>4</div>
+        </div>
+        <div class="side">
+          <div>Blender  </div>
+        </div>
+        <div class="middle">
+          <div class="bar-container">
+            <div class="bar-0"></div>
+          </div>
+        </div>
+        <div class="side right">
+          <div>3</div>
+        </div>
+      </div>
+      <hr class="my-4">
+      <p> Langue maternelle: Amharic</p>
+      <p> Franche:  niveau B1</p>
+      <p> English:   niveau B2</p>
+      <hr class="my-4">
+      <div class="my-4">
+     <div class="flooter">
+      <h2 >References</h2>
+      <p>Christine Theround, Bénévole à Pouvoir d'agir (chrstherond@yahoo.com)</p>
+      <p>Daniel Peppicelli, Ingénieur en informatique EPFL (daniel.peppicelli@gmail.com)</p></div></div>
+    </main>
+    <br>
+    <footer class="mastfoot mt-auto">
+    </footer>
   </div>
-</template>
 
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
   }
-}
 </script>
 
 <style>
 #app {
+  
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
